f167d45251ab41410974c1bfb6ad725f2d9add2f
Nurzhan Abdrassilov <nurzhan11abdrassilov@gmail.com>
2025-07-14 20:11:30 -0700

    completed second challenge : UUIDv4 approach

--- maelstrom-unique-ids/main.go
+++ maelstrom-unique-ids/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	//"crypto/sha256"
 	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"log"
-	"strconv"
-	"time"
+
+	//"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -14,7 +15,7 @@ import (
 func main() {
 	n := maelstrom.NewNode()
 	//var cnt int = 0
-	var id_str string = n.ID()
+	//var id_str string = n.ID()
 	//h := sha256.New()
 	n.Handle("generate", func(msg maelstrom.Message) error {
 		// Unmarshal the message body as an loosely-typed map.
@@ -27,13 +28,13 @@ func main() {
 		body["type"] = "generate_ok"
 
 		//h.Reset()
-		timestamp := time.Now().UnixNano()
+		//timestamp := time.Now().UnixNano()
 		//h.Write([]byte(timestamp + "/" + id_str + "%" + strconv.Itoa(cnt)))
 		//hashed := h.Sum(nil)
-		rand_num := make([]byte, 8) // 8 random bytes
+		rand_num := make([]byte, 16) // 8 random bytes
 		rand.Read(rand_num)
-		body["id"] = strconv.FormatInt(timestamp, 10) + "/" + id_str + "%" + string(rand_num)
-
+		//body["id"] = strconv.FormatInt(timestamp, 10) + "/" + id_str + "%" + string(rand_num)
+		body["id"] = hex.EncodeToString(rand_num)
 		//cnt += 1
 		// Echo the original message back with the updated message type.
 		return n.Reply(msg, body)
