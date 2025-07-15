eb3ac8c56b24d03478b231e5234bc42d47e74401
Nurzhan Abdrassilov <nurzhan11abdrassilov@gmail.com>
2025-07-14 20:03:14 -0700

    completed second challenge : intuitive approach

---
+++ maelstrom-unique-ids/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	//"crypto/sha256"
+	"crypto/rand"
+	"encoding/json"
+	"log"
+	"strconv"
+	"time"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+	//var cnt int = 0
+	var id_str string = n.ID()
+	//h := sha256.New()
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// Update the message type to return back.
+		body["type"] = "generate_ok"
+
+		//h.Reset()
+		timestamp := time.Now().UnixNano()
+		//h.Write([]byte(timestamp + "/" + id_str + "%" + strconv.Itoa(cnt)))
+		//hashed := h.Sum(nil)
+		rand_num := make([]byte, 8) // 8 random bytes
+		rand.Read(rand_num)
+		body["id"] = strconv.FormatInt(timestamp, 10) + "/" + id_str + "%" + string(rand_num)
+
+		//cnt += 1
+		// Echo the original message back with the updated message type.
+		return n.Reply(msg, body)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
